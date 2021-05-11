9cb02277c8cb83f2e3c82bb59b9e740c549a42c3
Ochaart <ochaart@users.noreply.github.com>
2021-05-11 13:58:24 -0700

    added snap to top of page after clicking home

--- client/src/components/navigation.jsx
+++ client/src/components/navigation.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import NavigationContainer from '../styles/navigation';
 
-const Navigation = () => (
-  <NavigationContainer>
-    <Link className="link" to={{ pathname: '/lazychef/home' }}>Home</Link>
-    {/* <Link className="link" to={{ pathname: '/lazychef/meal' }}>Meal Plan</Link> */}
-    <Link className="link" to={{ pathname: '/lazychef/cookbook' }}>Cook Book</Link>
-    {/* <Link className="link" to={{ pathname: '/lazychef/grocery' }}>Grocery Cart</Link> */}
-    {/* <Link className="link" to={{ pathname: '/lazychef/account' }}>My Account</Link> */}
-  </NavigationContainer>
-);
+const Navigation = () => {
+  const toTheTop = () => {
+    window.scrollTo(0, 0);
+  };
+  return (
+    <NavigationContainer>
+      <Link className="link" onClick={toTheTop} to={{ pathname: '/lazychef/home' }}>Home</Link>
+      {/* <Link className="link" to={{ pathname: '/lazychef/meal' }}>Meal Plan</Link> */}
+      <Link className="link" to={{ pathname: '/lazychef/cookbook' }}>Cook Book</Link>
+      {/* <Link className="link" to={{ pathname: '/lazychef/grocery' }}>Grocery Cart</Link> */}
+      {/* <Link className="link" to={{ pathname: '/lazychef/account' }}>My Account</Link> */}
+    </NavigationContainer>
+  );
+};
 
 export default Navigation;
